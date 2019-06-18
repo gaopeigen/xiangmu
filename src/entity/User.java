b7597fb37300fc37ba73d1168d4ab677db068e7d
gaopeigen <411001018@qq.com>
2019-06-18 09:05:32 +0800

    配置文件连接数据库使用ssm框架搭建项目

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,5 +1,26 @@
 package entity;
 
 public class User {
+	private String username;
+	private Integer id;
+	private Integer sex;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getSex() {
+		return sex;
+	}
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
 	
 }
